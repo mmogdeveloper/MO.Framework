af1fb2516d27a364988d42ee83232a9aef19fd97
mmogdeveloper <mmogdeveloper@hotmail.com>
2020-09-29 21:56:50 +0800

    unity3d功能完善

--- MO.Client/Unity3d/Assets/Scripts/Network/Actions/Gate100002Callback.cs
+++ MO.Client/Unity3d/Assets/Scripts/Network/Actions/Gate100002Callback.cs
@@ -1,8 +1,10 @@
 ﻿using GameFramework.Network;
+using MO.Protocol;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Assets.Scripts.Network.Actions
 {
@@ -15,7 +17,19 @@ namespace Assets.Scripts.Network.Actions
 
         public void Handle(object sender, Packet packet)
         {
+            S2C100002 rep = S2C100002.Parser.ParseFrom(((MOPacket)packet).Packet.Content);
+            if (rep.UserId == GameUser.Instance.CurPlayer.UserId)
+                return;
 
+            if (!GameUser.Instance.ViewPlayers.ContainsKey(rep.UserId))
+            {
+                var newPlayer = new PlayerData();
+                newPlayer.UserId = rep.UserId;
+                newPlayer.GameObject = (GameObject)Resources.Load("Player");
+                GameObject prefabInstance = GameObject.Instantiate(newPlayer.GameObject);
+                prefabInstance.transform.parent = GameObject.Find("Canvas").gameObject.transform;
+                GameUser.Instance.ViewPlayers.Add(rep.UserId, newPlayer);
+            }
         }
     }
 }

177483f1f105f4616a8af3eecf135a1fc83f3565
mmogdeveloper <mmogdeveloper@hotmail.com>
2020-10-02 07:23:25 +0800

    去掉测试示例

--- MO.Client/Unity3d/Assets/Scripts/Config/GameConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-public static class GameConfig
-{
-    public const string Md5Key = "8B203C98ADD54BACA9857C5DC4DFF603";
-}
--- MO.Client/Unity3d/Assets/Scripts/Network/NetworkUserData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-public class NetworkUserData
-{
-    public int ActionId { get; set; }
-}

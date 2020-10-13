1e6d44a738964ed18f55f59572898260a9648c14
mmogdeveloper <mmogdeveloper@hotmail.com>
2020-10-13 10:32:25 +0800

    去掉多余代码

--- MO.Servers/MO.GrainInterfaces/Config/GameConstants.cs
+++ MO.Servers/MO.GrainInterfaces/Config/GameConstants.cs
@@ -3,11 +3,10 @@
     public class GameConstants
     {
         /// <summary>
-        /// 对整个系统分区,1000节点一个区
+        /// 初始UserId
         /// </summary>
-        public const int GlobalCount = 1;
+        public const long USERIDINIT = 1380001;
 
-        public const long USERIDINIT = 1380001;//初始UserId
         /// <summary>
         /// Token过期时间
         /// </summary>
